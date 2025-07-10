aac75de5eabfd4d71b0bce3849c3c85277c1c507
mrcoat2 <27ajacobsen@go.dsdmail.net>
2025-07-09 23:21:29 -0600

    Improvements on speed and server logic

--- scan.py
+++ scan.py
@@ -1,8 +1,18 @@
 import nmap
 import requests # type: ignore
 import json
+import random
+import ipaddress
+from concurrent.futures import ThreadPoolExecutor
 
 MAIN_SERVER = "http://localhost:8080/"
+
+def get_rand_ip():
+    while True:
+        ip = ipaddress.IPv4Address(random.randint(0, 2**32 -1))
+        if not (
+            ip.is_private or ip.is_loopback or ip.is_multicast or ip.is_reserved or ip.is_link_local):
+            return str(ip)
 
 def confirm(message, choice):
     chosen = choice
@@ -17,31 +27,32 @@ def confirm(message, choice):
         return chosen.lower()
 
 def check_up(ips: str):
-    print("Starting scanner")
     new_scanner = nmap.PortScanner()
-    n = new_scanner.scan(hosts=ips, arguments='-n -T4 --min-parallelism 100 --max-retries 1')
+    n = new_scanner.scan(hosts=ips, arguments='-sV -n -T4 --min-parallelism 100 --max-retries 1')
 
     headers = {
         "scanned": ips
     }
 
+    if not 'tcp' in json.dumps(new_scanner[ips]):
+        return
+
     if n['nmap']['scanstats']['uphosts'] == '1':
         print(str(new_scanner[ips]))
         requests.post(MAIN_SERVER+"ips", data=json.dumps(new_scanner[ips]),headers=headers)
     
-
-def work():
-    to_do = requests.get(MAIN_SERVER + "todo")
-    print(to_do.text)
-
-    check_up(to_do.text)
+def create_worker_threads(count=200, threads=50):
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        for _ in range(count):
+            ip = get_rand_ip()
+            executor.submit(check_up, ip)
 
 def main():
     if confirm("This will start the scanner, start?", 'y') != 'y':
         exit()
     
-    while(True):
-        work()
+    while True:
+        create_worker_threads()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -2,7 +2,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import sqlite3
 import json
-import ast
+import ipaddress
+import requests
 
 conn = sqlite3.connect("ports.db")
 cursor = conn.cursor()
@@ -24,7 +25,8 @@ CREATE TABLE IF NOT EXISTS tcp_ports (
 """)
 conn.commit()
 
-current_address = "1.1.1.48"
+current_address = "1.1.3.244"
+
 
 def get_next_ip():
     global current_address
@@ -52,6 +54,13 @@ def sql_escape(val):
         return 'NULL'
     return "'" + str(val).replace("'", "''") + "'"
 
+def get_location(ip):
+    req = requests.get("https://ipinfo.io/"+ip+"/json")
+    loaded = json.loads(req.text)
+    location = f"{loaded['city']}, {loaded['region']}, {loaded['country']}"
+    
+    return location
+
 class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path == "/ips":
@@ -59,11 +68,24 @@ class MyHandler(BaseHTTPRequestHandler):
             length = int(self.headers.get('Content-Length', 0))
             
             data = self.rfile.read(length).decode()
-            data = json.loads(data)
+
+            try:
+                data = json.loads(data)
+            except json.JSONDecodeError:
+                self.send_error(400, "Invalid JSON")
+                return
 
             for port, info in data['tcp'].items():
+                if info.get('state') == 'closed' or info.get('state') == 'filtered':
+                    continue
+                
+                if info.get('name') == 'tcpwrapped':
+                    continue
+
+                loc = get_location(self.headers.get('scanned', 0))
+
                 sql = f"""INSERT INTO tcp_ports (
-                    ip, port, state, reason, name, product, version, extrainfo, conf, cpe, scripts
+                    ip, port, state, reason, name, product, version, extrainfo, conf, cpe, scripts, location
                 ) VALUES (
                     {sql_escape(self.headers.get('scanned', 0))},
                     {port},
@@ -75,7 +97,8 @@ class MyHandler(BaseHTTPRequestHandler):
                     {sql_escape(info.get('extrainfo'))},
                     {sql_escape(info.get('conf'))},
                     {sql_escape(info.get('cpe'))},
-                    {sql_escape(json.dumps(info.get('script', {})))}
+                    {sql_escape(json.dumps(info.get('script', {})))},
+                    {sql_escape(loc)}
                 );"""
                 cursor.execute(sql)
                 conn.commit()
@@ -101,10 +124,11 @@ class MyHandler(BaseHTTPRequestHandler):
             self.send_error(404, "Not Found")
 
 def run(server_class=HTTPServer, handler_class=MyHandler, port=8080):
-    server = server_class(('', port), handler_class)
+    server = server_class(('0.0.0.0', port), handler_class)
     print(f"Listening on port {port}...")
     server.serve_forever()
 
 if __name__ == "__main__":
+    get_next_ip()
     run()
     conn.close()
